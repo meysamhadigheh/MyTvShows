702b6e65b2a7f6962befe10ea4b7cfbfc78ff350
Meysam Hadigheh <hadigheh.meysam@gmail.com>
2022-02-05 01:54:04 +0100

    migrate MainActivity.kt

--- app/src/main/java/info/meysam/mytvshows/ui/view/activities/MainActivity.kt
+++ app/src/main/java/info/meysam/mytvshows/ui/view/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package info.meysam.mytvshows
+package info.meysam.mytvshows.ui.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import info.meysam.mytvshows.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
